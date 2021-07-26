87f96770991468d9005e9cbf5f7702e08647deb3
cho.jardin <cho.jardin@gmail.com>
2021-07-26 17:46:33 +0900

    deploy;

---
+++ Server_django/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [".us-east-2.compute.amazonaws.com",
+                 'ec2-3-15-7-90.us-east-2.compute.amazonaws.com'
+                 "3.15.7.90"]

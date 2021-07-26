eb5b39ee010dcddbc436a4bf8d80d6fd0ecb4be8
cho.jardin <cho.jardin@gmail.com>
2021-07-26 18:10:53 +0900

    deploy

--- Server_django/settings/production.py
+++ Server_django/settings/production.py
@@ -4,4 +4,5 @@ DEBUG = False
 
 ALLOWED_HOSTS = [".us-east-2.compute.amazonaws.com",
                  'ec2-3-15-7-90.us-east-2.compute.amazonaws.com'
-                 "3.15.7.90"]
+                 "3.15.7.90",
+                 'ssatcha.site']
